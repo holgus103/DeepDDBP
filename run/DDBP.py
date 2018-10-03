e806880af442e2ff2e41207c96d918e88ff03787
holgus103 <holgus103@gmail.com>
2018-10-03 19:34:19 +0200

    Missing files

--- run/DDBP.py
+++ run/DDBP.py
@@ -1,5 +1,6 @@
 
 import sys;
+import numpy;   
 
 import tensorflow as tf
 
@@ -31,7 +32,13 @@ path = "./summaries/{0}/".format(experiment_name);
 dp.initialize_random(experiment_name);
 
 # import data
-(data, labels, test_data, test_labels) = dp.read_file("./../data/library", SET_SIZE, True, TRAIN_NO_TRUMP, TRAIN_TRUMP, TEST_NO_TRUMP, TEST_TRUMP, PARTITION);
+#(data, labels, test_data, test_labels) = dp.read_file("./../data/library", SET_SIZE, True, TRAIN_NO_TRUMP, TRAIN_TRUMP, TEST_NO_TRUMP, TEST_TRUMP, PARTITION);
+data = numpy.load(path + "train_data.npy");
+labels = numpy.load(path + "train_output.npy");
+test_data = numpy.load(path + "test_data.npy");
+test_labels = numpy.load(path + "test_output.npy");
+finetuning_outputs = numpy.load(path + "finetuningtest_outputs.npy")
+comparables = dp.labeled_dictionary(data, labels, 3);
 (samples_l, samples_r, outputs, diffs) = dp.generate_balanced_classes(data, labels, 200, 400, 200);
 (test_samples_l, test_samples_r, test_outsputs, test_diffs) = dp.generate_balanced_classes(test_data, test_labels, 66, 66, 66);
 
@@ -72,7 +79,7 @@ a.pretrain(0.001, 0, 1000, data_batches, 0, 0.1, path + "{0}" , optimizer, 0.2, 
 # create classifier
 c = models.Classifier(a, 2);
 # train whole network
-c.train(data_batches_l, data_batches_r, outputs_batches, 0.0001, 15000, 0.0001, path +"/finetuning", samples_l, samples_r, outputs, diffs, test_samples_l, test_samples_r, test_outsputs, test_diffs, dp.suit_count_for_params(TRAIN_NO_TRUMP, TRAIN_TRUMP), dp.suit_count_for_params(TEST_NO_TRUMP, TEST_TRUMP), models.Model.mse_loss, 25, experiment_name);
+c.train(data_batches_l, data_batches_r, outputs_batches, 0.0001, 15000, 0.0001, path +"/finetuning", samples_l, samples_r, outputs, diffs, test_samples_l, test_samples_r, test_outsputs, test_diffs, test_data, test_labels, finetuning_outputs, comparables, dp.suit_count_for_params(TRAIN_NO_TRUMP, TRAIN_TRUMP), dp.suit_count_for_params(TEST_NO_TRUMP, TEST_TRUMP), models.Model.mse_loss, 25, experiment_name);
 
 
 # evaluate results
